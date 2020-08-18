1372316e682f86c635f9fdf7713caf0e0992d709
Vrazhnov <www.euroman@mail.ru>
2020-08-18 12:52:51 +0300

    Correct annotation

--- src/test/java/test/test/FirstTest.java
+++ src/test/java/test/test/FirstTest.java
@@ -1,62 +1,40 @@
 package test.test;
-import org.junit.Test;
-        import org.junit.Before;
-        import org.junit.After;
-        import static org.junit.Assert.*;
-        import static org.hamcrest.CoreMatchers.is;
-        import static org.hamcrest.core.IsNot.not;
-        import org.openqa.selenium.By;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.firefox.FirefoxDriver;
-        import org.openqa.selenium.chrome.ChromeDriver;
-        import org.openqa.selenium.remote.RemoteWebDriver;
-        import org.openqa.selenium.remote.DesiredCapabilities;
-        import org.openqa.selenium.Dimension;
-        import org.openqa.selenium.WebElement;
-        import org.openqa.selenium.interactions.Actions;
-        import org.openqa.selenium.support.ui.ExpectedConditions;
-        import org.openqa.selenium.support.ui.WebDriverWait;
-        import org.openqa.selenium.JavascriptExecutor;
-        import org.openqa.selenium.Alert;
-        import org.openqa.selenium.Keys;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.TestRunner;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.Console;
-import java.util.*;
-        import java.net.MalformedURLException;
-        import java.net.URL;
 public class FirstTest {
     private WebDriver driver;
     private Map<String, Object> vars;
     JavascriptExecutor js;
-
-
 
     @BeforeTest
     public void setUp() {
         driver = new FirefoxDriver();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
-
+//        driver.manage().window().setSize(new Dimension(1936, 1066));
+//        login();
     }
+
     @AfterTest
     public void tearDown() {
         driver.quit();
     }
 
-    @org.testng.annotations.Test (priority = 1)
+    @Test(priority = 1)
     public void login() {
         driver.get("http://tos2.solvo.ru:37580/aet/login.xhtml");
-        driver.manage().window().setSize(new Dimension(1936, 1066));
+//        driver.manage().window().setSize(new Dimension(1936, 1066));
         driver.findElement(By.id("LoginForm:userid")).click();
-        driver.findElement(By.id("LoginForm:userid")).sendKeys("olegatorw");
-        driver.findElement(By.id("LoginForm:password")).sendKeys("123");
+        driver.findElement(By.id("LoginForm:userid")).sendKeys("ROBOTESTER");
+        driver.findElement(By.id("LoginForm:password")).sendKeys("ELECTROSTALIN");
         driver.findElement(By.id("LoginForm:language")).click();
         driver.findElement(By.id("LoginForm:language_label")).click();
         driver.findElement(By.id("LoginForm:language_0")).click();
@@ -73,8 +51,7 @@ public class FirstTest {
             System.out.println("There was no active login");
         }
     }
-
-    @org.testng.annotations.Test (priority = 2)
+    @Test(priority = 2)
     public void makeBoLWithGC() {
         driver.get("http://tos2.solvo.ru:37580/aet/private/bill_of_lading.xhtml?id=1014241");
         //driver.findElement(By.linkText("Processing")).click(); // TODO: Examine why tf it isn't working
